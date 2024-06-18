b36960f6d7cb605fc54157707fd221ef75889bf3
DDMDMD Dmytroe <davidenkomail@gmail.com>
2024-06-17 21:09:29 -0400

    update

--- src/Components/api/imageApi.js
+++ src/Components/api/imageApi.js
@@ -1,6 +1,11 @@
 import axios from "axios";
+// import dotenv from "dotenv";
+// dotenv.config();
+
 axios.defaults.baseURL = "https://api.unsplash.com/";
-const YOUR_ACCESS_KEY = "UUFtwa-HkEpVUiD29u9gHnt2hbZanw2eefQt1P99Ab4";
+const YOUR_ACCESS_KEY = process.env.REACT_APP_YOUR_ACCESS_KEY;
+
+// const YOUR_ACCESS_KEY = "UUFtwa-HkEpVUiD29u9gHnt2hbZanw2eefQt1P99Ab4";
 export const getImages = async (searchQuery, page) => {
   const data = await axios.get("search/photos/", {
     params: {
